ab84097d05a3f735772864186c5ee9143556634c
daniel gomez <gomezdespen@gmail.com>
2020-12-22 14:15:50 -0500

    lineGraph/Stats

--- src/Stats.js
+++ src/Stats.js
@@ -4,7 +4,23 @@ import './Stats.css';
 function Stats() {
     return (
         <div className="stats">
-            <div className="">
+            <div className="stats__container">
+                <div className="stats__header">
+                    <p>Stocks</p>
+                </div>
+                <div className="stats__content">
+                    <div className="stats__row">
+
+                    </div>
+                </div>
+                <div className="stats__header">
+                    <p>Lists</p>
+                </div>
+                <div className="stats__content">
+                    <div className="stats__row">
+
+                    </div>
+                </div>
 
             </div>
             
