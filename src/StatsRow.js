4cbf83824f2f4b03e7cae175b13ae788b379c7c0
daniel gomez <gomezdespen@gmail.com>
2021-01-20 11:57:53 -0500

    stats row /  dynamic info

--- src/StatsRow.js
+++ src/StatsRow.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './StatsRow.css';
+import StockSVG from './stock.svg';
  
-function StatsRow() {
+function StatsRow(props) {
+
+    const percentage = ((props.price - props.openPrice)/props.openPrice) * 100;
+
     return (
         <div className="row">
             <div className="row__intro">
-                <h1>AAPL</h1>
-                <p>200 shares</p>
+
+                <h1>{props.names}</h1>
+
+                <p>{props.shares && 
+                    (props.shares + "shares")
+                    }
+                </p>
+
             </div>
             <div className="row__chart">
-                <img src="{StockChart}" alt="#" height={16}/>
+                <img src={StockSVG} alt="#" height={16}/>
             </div>
             <div className="row__numbers">
-                <p className="row__price">$200</p>
-                <p className="row__percentage"> +200%</p>
+                <p className="row__price">{props.price}</p>
+                <p className="row__percentage">{Number (percentage).toFixed(2)}%</p>
             </div>
             
         </div>
@@ -21,3 +31,4 @@ function StatsRow() {
 }; 
 
 export default StatsRow;
+ 
